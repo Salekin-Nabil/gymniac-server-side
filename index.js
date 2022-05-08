8bca15ad461af4623f8eb2d3c17b8e2436f441a3
Salekin-Nabil <salekinnabil@gmail.com>
2022-05-08 16:14:00 +0600

    trying pagination

--- index.js
+++ index.js
@@ -74,6 +74,33 @@ async function run() {
                 res.status(403).send({message: 'forbidden access'})
             }
         })
+        //GET API for Pagination
+        app.get('/product', async(req, res) =>{
+            console.log('query', req.query);
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if(page || size){
+                // 0 --> skip: 0 get: 0-10(10): 
+                // 1 --> skip: 1*10 get: 11-20(10):
+                // 2 --> skip: 2*10 get: 21-30 (10):
+                // 3 --> skip: 3*10 get: 21-30 (10):
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray();
+            }
+            
+            res.send(products);
+        });
+        //GET API Pagination
+        app.get('/productCount', async(req, res) =>{
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count});
+        });
         //Products POST API
         app.post('/products', async (req, res)=>{
             const newProduct = req.body;
